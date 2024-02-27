04c80025cef535617158626c2cfe34609a79b3cc
Sebastian Mößler <s.moeszler@binsoul.de>
2024-02-27 11:04:46 +0100

    Add compiler pass which maps Doctrine entities

--- src/BinsoulWebsiteBundle.php
+++ src/BinsoulWebsiteBundle.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace BinSoul\Symfony\Bundle\Website;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BinsoulWebsiteBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+
+        if (class_exists($ormCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createAttributeMappingDriver(
+                    ['BinSoul\Symfony\Bundle\Website'],
+                    [realpath(__DIR__.'/src/Entity')],
+                )
+            );
+        }
+    }
 }
