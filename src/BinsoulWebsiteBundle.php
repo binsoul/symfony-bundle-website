daad26302cb81fc7be4cf35e15ed6306b851d7fc
Sebastian Mößler <s.moeszler@binsoul.de>
2024-10-15 16:55:26 +0200

    Don't use provided method to create DoctrineOrmMappingsPass

--- src/BinsoulWebsiteBundle.php
+++ src/BinsoulWebsiteBundle.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace BinSoul\Symfony\Bundle\Website;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BinsoulWebsiteBundle extends Bundle
@@ -14,15 +16,19 @@ class BinsoulWebsiteBundle extends Bundle
     {
         parent::build($container);
 
-        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
-
-        if (class_exists($ormCompilerClass)) {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createAttributeMappingDriver(
-                    ['BinSoul\Symfony\Bundle\Website'],
-                    [(string) realpath(__DIR__ . '/Entity')],
-                )
-            );
-        }
+        $container->addCompilerPass(
+            $this->buildDoctrineOrmMappingsPass(
+                ['BinSoul\Symfony\Bundle\Website'],
+                [(string) realpath(__DIR__ . '/Entity')],
+            )
+        );
     }
+
+    private function buildDoctrineOrmMappingsPass(array $namespaces, array $directories): DoctrineOrmMappingsPass
+    {
+        $driver = new Definition(AttributeDriver::class, [$directories]);
+
+        return new DoctrineOrmMappingsPass($driver, $namespaces, [], false, []);
+    }
+
 }
